216459d179edbe61683d5029d71b95c5b4fcae09
LexTech <ryanvalentin288@gmail.com>
2023-07-21 14:44:59 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,159 @@
+import { TelegramClient, Api } from "telegram";
+import { StringSession } from "telegram/sessions/index.js";
+import express from "express";
+import https from "https";
+
+import ffmpeg from "fluent-ffmpeg";
+
+import dotenv from "dotenv";
+import fs, { promises as fsPromises } from "fs";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.static("./downloads"));
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(3000, () => {
+    console.log("listening");
+});
+
+const apiId = parseInt(process.env.APP_API_ID);
+const apiHash = process.env.APP_API_HASH;
+
+const stringSession = process.env.APP_SESSION; // leave this empty for now
+const BOT_TOKEN = process.env.bot; // put your bot token here
+
+(async () => {
+    const client = new TelegramClient(
+        new StringSession(stringSession),
+        apiId,
+        apiHash,
+        { connectionRetries: 5 }
+    );
+
+    if (stringSession) {
+        await client.start();
+    } else {
+        await client.start({
+            botAuthToken: BOT_TOKEN,
+        });
+
+        console.log(client.session.save());
+    }
+
+    client.addEventHandler(async (update) => {
+        const chatID = Number(update.message.chatId);
+
+        if (update.message.message.startsWith("/start")) {
+            await client.sendMessage(chatID, {
+                message: "Welcome to my Telegram bot!",
+            });
+        }
+
+        if (
+            update.message.message.startsWith("https") ||
+            update.message.message.startsWith("http")
+        ) {
+            const videoURL = update.message.message;
+
+            await client.sendMessage(chatID, {
+                message: "Downloading Video",
+            });
+
+            if (!fs.existsSync("./downloads")) {
+                await fsPromises.mkdir("./downloads");
+            }
+
+            const fileName = path.basename(videoURL);
+            const filePath = `./downloads/${fileName}`;
+
+            fs.closeSync(fs.openSync(filePath, "w"));
+
+            const file = fs.createWriteStream(filePath);
+
+            const request = https.get(videoURL, async function (response) {
+                const totalLength = response.headers["content-length"];
+                let downloadedLength = 0;
+
+                await client.sendMessage(chatID, {
+                    message: "Video size: " + totalLength / 1024 / 1024,
+                });
+
+                response.on("data", function (chunk) {
+                    downloadedLength += chunk.length;
+                    console.log(
+                        `Downloaded ${(
+                            (downloadedLength / totalLength) *
+                            100
+                        ).toFixed(2)}%`
+                    );
+                });
+
+                response.pipe(file);
+            });
+
+            file.on("finish", async function () {
+                if (!fs.existsSync("./thumbnail")) {
+                    await fsPromises.mkdir("./thumbnail");
+                }
+
+                ffmpeg.ffprobe(filePath, (error, metadata) => {
+                    console.log("done", filePath);
+                    const duration = metadata.format.duration;
+                    console.log(duration);
+
+                    const timeIndex = "00:00:10";
+
+                    ffmpeg(filePath)
+                        .on("end", async function () {
+                            await client.sendFile(chatID, {
+                                file: filePath,
+                                caption: fileName,
+                                thumb: "./thumbnail/thumbnail.jpg",
+                                attributes: [
+                                    new Api.DocumentAttributeVideo({
+                                        w: 640,
+                                        h: 480,
+                                        supportsStreaming: true,
+                                        duration,
+                                    }),
+                                ],
+                                workers: 1,
+                                progressCallback: (pro) => {
+                                    console.log(
+                                        "Uploadind Torrent: " + pro * 100 + "%"
+                                    );
+                                },
+                            });
+
+                            await fsPromises.rm(`./downloads/${fileName}`);
+                            await fsPromises.rm(`./thumbnail/thumbnail.jpg`);
+
+                            await client.sendMessage(chatID, {
+                                message: "You can send new url",
+                            });
+                        })
+                        .on("error", function (err) {
+                            console.error(err);
+                        })
+                        .screenshots({
+                            count: 1,
+                            timemarks: [timeIndex],
+                            filename: "thumbnail.jpg",
+                            folder: "./thumbnail",
+                        });
+                });
+            });
+        }
+    });
+})();
+
+process.on("uncaughtException", (err) => {
+    console.log(err);
+});
